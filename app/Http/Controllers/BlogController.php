4469bf53f50eaa3dd4f54b9be6fe7fb470bfe230
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2026-03-09 15:16:24 +0800

    Add authentication middleware to BlogController and implement store method for blog creation. Update navigation in app layout to include links for viewing and creating blogs.

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $blogs = \App\Models\Blog::all();
@@ -15,5 +20,17 @@ class BlogController extends Controller
     public function create()
     {
         return view('blogs.create'); // resources/views/blogs/create.blade.php
+    }
+
+    public function store(Request $request)
+    {
+        // POPO - Plain Old PHP Object
+        $blog = new \App\Models\Blog();
+
+        $blog->title = $request->title;
+        $blog->content = $request->content;
+        $blog->user_id = auth()->user()->id;
+        $blog->save();
+        return redirect()->route('blogs.index');
     }
 }
