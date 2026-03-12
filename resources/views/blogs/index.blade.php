7557581ef1b3d3fa9647cdcadae269049663e716
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2026-03-12 15:31:56 +0800

    Add new error pages (401, 404, 500) and update navigation links in the sidebar for error handling. Introduce login, password reset, and registration pages for user authentication. Implement static and light sidenav layouts for improved UI navigation.

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,9 +1,13 @@
-@extends('layouts.app')
+@extends('admin.layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Blog Index</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Blogs Index</li>
+    </ol>
+    <div class="row">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Blogs Index') }}</div>
 
